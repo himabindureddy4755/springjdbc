4d5900b4c7ae37c5034ba3ca0e57afe125909096
hp <bindureddy4755@gmail.com>
2021-04-22 19:15:14 +0530

    initial commit

---
+++ springjdbc/src/basic/PlayJdbc.java
@@ -0,0 +1,11 @@
+package basic;
+
+import basic.model.Student;
+
+public class PlayJdbc {
+	public static void main(String[] args) {
+		Student myStudent = new Student(3, "himani", 5, 66);
+		Student anotherStudent = new Student(4,"harshi",6,88);
+	}
+
+}
